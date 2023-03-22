f002c5617746e4c155db219db2b9916e449d7104
Graeme Paterson <ghpaterson@gmail.com>
2023-03-22 15:09:36 +0000

    useAuthState, useEffect checks if there is a user and redirects to homepage

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,15 +1,31 @@
 import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../utils/firebase";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 export default function Login() {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
   //signin with google
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
-    } catch (error) {}
+      route.push("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    if (user) {
+      route.push("/");
+    } else {
+      console.log("login please");
+    }
+  }, [user]);
 
   return (
     <div className="shadow-xl mt-32 p-10 textgray-800 rounded-lg">
