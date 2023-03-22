81fac66080fe53b4bb52fd2d900f1a27f88a885c
Graeme Paterson <ghpaterson@gmail.com>
2023-03-22 15:00:37 +0000

    implemented the sign in with google feature

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,12 +1,25 @@
 import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utils/firebase";
 
 export default function Login() {
+  //signin with google
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+    } catch (error) {}
+  };
+
   return (
     <div className="shadow-xl mt-32 p-10 textgray-800 rounded-lg">
       <h2 className="text-2xl font-medium">Join Today</h2>
       <div className="py-4">
         <h3 className="py-4">Sign in with a Provider</h3>
-        <button className="text-white bg-gray-800 w-full font-medium rounded-lg flex align-middle p-4 gap-2">
+        <button
+          onClick={GoogleLogin}
+          className="text-white bg-gray-800 w-full font-medium rounded-lg flex align-middle p-4 gap-2"
+        >
           <FcGoogle className="text-2xl" />
           Sign in with Google
         </button>
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase";
-import { getFireStore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,7 +12,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
@@ -22,4 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth();
-export const db = getFireStore(app);
+export const db = getFirestore(app);
