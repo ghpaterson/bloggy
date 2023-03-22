dd5aa81d30a81d0cb0a28cfa86f7d6973912c5af
Graeme Paterson <ghpaterson@gmail.com>
2023-03-22 11:45:24 +0000

    continued with setup, imported font and cleaned up globals.css

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import NavBar from "./navBar";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="font-poppins mx-6 md:max-w-2xl md:mx-auto">
       <NavBar />
       <main>{children}</main>
     </div>
