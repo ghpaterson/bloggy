f30b109af1b40d7597fb6f68b122c5390d704d97
Graeme Paterson <ghpaterson@gmail.com>
2023-03-23 16:17:20 +0000

    added the post, timestamp, user id and username to the firebase db

--- pages/music.js
+++ pages/music.js
@@ -1,15 +1,28 @@
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Auth } from "../utils/firebase";
+import { auth, db } from "../utils/firebase";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 export default function Music() {
   //form state
   const [post, setPost] = useState({ description: "" });
+  const [user, loading] = useAuthState(auth);
+  const route = useRouter();
 
   //submit post
   const submitPost = async (e) => {
     e.preventDefault();
+    //make a new post
+    const collectionRef = collection(db, "posts");
+    await addDoc(collectionRef, {
+      ...post,
+      timestamp: serverTimestamp(),
+      user: user.uid,
+      username: user.displayName,
+    });
+    setPost({ description: "" });
+    return route.push("/");
   };
 
   return (
