7f6acc109c6124b5805f5e16720e29a8a0abac78
Graeme Paterson <ghpaterson@gmail.com>
2023-03-23 15:51:53 +0000

    form state and submit post function, 300 word count limit

--- pages/music.js
+++ pages/music.js
@@ -4,16 +4,34 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Music() {
+  //form state
+  const [post, setPost] = useState({ description: "" });
+
+  //submit post
+  const submitPost = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
-      <form>
+      <form onSubmit={submitPost}>
         <h1>New Post</h1>
         <div className="py-2">
           <h3>Description</h3>
-          <textarea className="bg-gray-800 h-48 w-full text-gray-100 rounded-lg p-2 text-sm"></textarea>
-          <p className="text-gray-300">0/300</p>
+          <textarea
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+            className="bg-gray-800 h-48 w-full text-gray-100 rounded-lg p-2 text-sm"
+          ></textarea>
+          <p
+            className={`text-cyan-600 ${
+              post.description.length > 300 ? "text-red-500" : ""
+            }`}
+          >
+            {post.description.length}/300
+          </p>
         </div>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
