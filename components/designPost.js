85431dde3e7d546d721a66adf4eea18d0e7a4675
Graeme Paterson <ghpaterson@gmail.com>
2023-03-28 16:34:38 +0100

    added #category under each post for easier identity

--- components/designPost.js
+++ components/designPost.js
@@ -5,6 +5,7 @@ export default function DesignPost({
   username,
   description,
   timestamp,
+  type,
 }) {
   const convertLinks = (text) => {
     const urlRegex = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/g;
@@ -22,6 +23,9 @@ export default function DesignPost({
     <div className="bg-gray-100 p-8 border-b-2 rounded-lg">
       <div className="flex items-center gap-2">
         <h2 className="text-md text-gray-800">{username}</h2>
+      </div>
+      <div>
+        <h3 className="text-sm text-gray-600">#{type}</h3>
       </div>
       <div className="py-4">
         <p
